05803ff80f473ed2e61be6b0132470242e48842f
siddutgikar <siddharthutgikar@gmail.com>
2016-04-30 20:57:49 -0400

    Development Saturday 30th April

--- app/src/androidTest/java/fragments/EventListFragment.java
+++ app/src/androidTest/java/fragments/EventListFragment.java
@@ -1,8 +1,8 @@
 package fragments;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +18,7 @@ public class EventListFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @Nullable
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
--- app/src/main/java/com/mobilecomputing/flee/flee/EventListActivity.java
+++ app/src/main/java/com/mobilecomputing/flee/flee/EventListActivity.java
@@ -3,44 +3,119 @@ package com.mobilecomputing.flee.flee;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.DisplayMetrics;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
 
 /**
  * Created by siddh on 4/22/2016.
  */
-public class EventListActivity extends FragmentActivity implements View.OnClickListener {
+public class EventListActivity extends FragmentActivity implements View.OnClickListener, Animation.AnimationListener {
 
-    //Asen test comit
+    /**
+     * Flag to decide when search panel can be seen
+     */
+    private boolean isSearchPanelVisible = false, isAnimating = false;
+    /**
+     * Animations for sliding of Search Panel
+     */
+    TranslateAnimation transIn;
+    TranslateAnimation transOut;
+
+    /**
+     * Search Panel Layout
+     */
+    LinearLayout lnSearchLayout;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventlist);
+        initView();
+
     }
 
+    /**
+     * Will initialise all the UI elements
+     */
+    private void initView() {
+        lnSearchLayout = (LinearLayout)findViewById(R.id.searchPanelLayout);
+
+        DisplayMetrics metrics = this.getResources()
+                .getDisplayMetrics();
+        int width = metrics.widthPixels;
+        transIn = new TranslateAnimation(width, 0, 0, 0);
+        transIn.setAnimationListener(this);
+        transIn.setDuration(300);
+        transOut = new TranslateAnimation(0, width, 0, 0);
+        transOut.setAnimationListener(this);
+        transOut.setDuration(300);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_eventlist, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * WIll animate the panel in and out
+     *
+     * @param direction will have 1 - for coming in 2 - for going out
+     */
+    public void animatePanel(int direction) {
+        switch (direction) {
+            case 1:
+                lnSearchLayout.bringToFront();
+                lnSearchLayout.clearAnimation();
+                lnSearchLayout.setAnimation(transIn);
+                lnSearchLayout.startAnimation(transIn);
+                lnSearchLayout.setVisibility(View.VISIBLE);
+                isSearchPanelVisible = true;
+                break;
+            case 2:
+                lnSearchLayout.clearAnimation();
+                lnSearchLayout.setAnimation(transOut);
+                lnSearchLayout.startAnimation(transOut);
+                isSearchPanelVisible = false;
+                lnSearchLayout.setVisibility(View.GONE);
+                break;
+        }
     }
 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.event_menusearch:
+                if (!isAnimating) {
+                    if (!isSearchPanelVisible) {
+                        animatePanel(1);
+                    } else {
+                        animatePanel(2);
+                    }
+                }
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 
     /**
      * On Click Listener for Buttons in the activity
+     *
      * @param v
      */
     @Override
     public void onClick(View v) {
-        switch(v.getId())
-        {
+        switch (v.getId()) {
 
 
         }
@@ -57,5 +132,18 @@ public class EventListActivity extends FragmentActivity implements View.OnClickL
     }
 
 
+    @Override
+    public void onAnimationStart(Animation animation) {
+        isAnimating = true;
+    }
 
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        isAnimating = false;
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
 }
