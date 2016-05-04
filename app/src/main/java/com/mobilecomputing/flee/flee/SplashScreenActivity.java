f581f2bc892dc0b4a979c5c90d0c7b5b469f2571
Amanda Pyryt <amandapyryt@gmail.com>
2016-05-04 18:13:28 -0400

    Completed splash screen activity

--- app/src/main/java/com/mobilecomputing/flee/flee/SplashScreenActivity.java
+++ app/src/main/java/com/mobilecomputing/flee/flee/SplashScreenActivity.java
@@ -1,18 +1,34 @@
 package com.mobilecomputing.flee.flee;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
+import android.widget.VideoView;
 
 /**
  * Created by siddh on 4/22/2016.
+ * Edited by apyryt on 5/4/16
  */
-public class SplashScreenActivity extends Activity {
+public class SplashScreenActivity extends Activity implements MediaPlayer.OnCompletionListener {
+
+    VideoView splash;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //loads the video file for the splashscreen
+        splash = (VideoView) findViewById(R.id.videoView);
+        Uri splashPath = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.flee1);
+        splash.setVideoURI(splashPath);
+        splash.start();
+        splash.setOnCompletionListener(this);
+
     }
 
     @Override
@@ -23,5 +39,15 @@ public class SplashScreenActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+        //When the splash screen finishes playing, start the next activity (login activity)
+        Intent startAppIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+        startActivity(startAppIntent);
+
+        finish();
     }
 }
