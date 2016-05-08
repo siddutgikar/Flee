acdd33138ecdffbfbd591b31ff00b7e26da54999
Amanda Pyryt <amandapyryt@gmail.com>
2016-05-08 15:16:44 -0400

    changed add event page files

--- app/src/main/java/com/mobilecomputing/flee/flee/AddActivity.java
+++ app/src/main/java/com/mobilecomputing/flee/flee/AddActivity.java
@@ -1,36 +1,65 @@
 package com.mobilecomputing.flee.flee;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 //created by apyryt on 5/7/16
 
 public class AddActivity extends Activity implements View.OnClickListener {
 
-    TextView name, location, date, time, category, description;
-    Button uploadImage, done;
+    private final static int SELECT_PHOTO = 12345;
+
+    private EditText name, location, date, time, category, description;
+    private TextView nameLabel, locationLabel, dateLabel, timeLabel, categoryLabel, descrLabel, addEventLabel;
+    private ImageButton uploadImage, done;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
+        setContentView(R.layout.add_event);
+
+        //create the custom font
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/FedraSansStd-Bold.ttf");
+
+        //initialize textViews
+        addEventLabel = (TextView) findViewById(R.id.textViewAddEvent);
+        nameLabel = (TextView) findViewById(R.id.nameTextField);
+        locationLabel = (TextView) findViewById(R.id.locationTextField);
+        dateLabel = (TextView) findViewById(R.id.dateTextField);
+        timeLabel = (TextView) findViewById(R.id.timeTextField);
+        categoryLabel = (TextView) findViewById(R.id.categoryTextField);
+        descrLabel = (TextView) findViewById(R.id.descrTextField);
+
+        //setting the font for the editTexts
+        addEventLabel.setTypeface(font);
+        nameLabel.setTypeface(font);
+        locationLabel.setTypeface(font);
+        dateLabel.setTypeface(font);
+        timeLabel.setTypeface(font);
+        categoryLabel.setTypeface(font);
+        descrLabel.setTypeface(font);
 
         //Just initializes the TextViews and buttons to be used for creating an event
-        name = (TextView) findViewById(R.id.nameTextField);
-        location = (TextView) findViewById(R.id.dateTextField);
-        date = (TextView) findViewById(R.id.timeTextField);
-        time = (TextView) findViewById(R.id.timeTextField);
-        category = (TextView) findViewById(R.id.categoryTextField);
-        description = (TextView) findViewById(R.id.descriptionText);
+        name = (EditText) findViewById(R.id.nameTextField);
+        location = (EditText) findViewById(R.id.dateTextField);
+        date = (EditText) findViewById(R.id.timeTextField);
+        time = (EditText) findViewById(R.id.timeTextField);
+        category = (EditText) findViewById(R.id.categoryTextField);
+        description = (EditText) findViewById(R.id.descrTextField);
 
-        uploadImage = (Button) findViewById(R.id.uploadImageButton);
+        uploadImage = (ImageButton) findViewById(R.id.uploadImageButton);
         uploadImage.setOnClickListener(this);
 
-        done = (Button) findViewById(R.id.doneButton);
+        done = (ImageButton) findViewById(R.id.doneButton);
         done.setOnClickListener(this);
 
     }
@@ -39,13 +68,25 @@ public class AddActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
 
-            //go to a select picture screen
+            //select a photo from the android photo album
             case R.id.uploadImageButton:
+                Intent photoPicker = new Intent(Intent.ACTION_PICK);
+                photoPicker.setType("image/*");
+                startActivityForResult(photoPicker, SELECT_PHOTO);
                 break;
 
             //create an event to add to the database
             case R.id.doneButton:
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK) {
+            Uri image = data.getData();
+
+
         }
     }
 }
